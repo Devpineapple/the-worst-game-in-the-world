92983d028410b795fd23e3dc539f2815a67233ab
T H A T  B O I <43853225+Devpineapple@users.noreply.github.com>
2020-03-28 15:15:17 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,145 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+f f f f f f f f f f f f f f f f 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 e e 7 7 7 e e 7 e e 7 7 e 7 e 
+7 e e 7 7 e e e 7 e e e 7 e 7 e 
+e e e e e e e e 7 e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+e e e e e e e e e e e e e e e e 
+e d e e e e e d e e e e e e e e 
+e e e e d e e e e e e e e e e e 
+e e d e e e e e e e e d e e e e 
+e e e e e e e e d e e e e e e e 
+e e e e e e e e e e e d e e e e 
+e e e e d e e e e e e e e e e e 
+e e e e e e e e e d e e e e d e 
+d e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e d e e e d e e e d e e 
+e e e e e e e e e e e e e e e e 
+e e d e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e d e e e e e e 
+e e e e e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 f f f f f f 9 9 9 
+9 9 9 9 9 f f 1 1 1 1 1 1 f 9 9 
+9 9 9 f f 1 1 1 1 1 1 1 1 1 f f 
+9 9 f f 1 1 1 1 1 1 1 1 1 1 1 f 
+9 9 f 1 1 1 1 1 1 1 1 1 1 1 1 f 
+9 f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+1 1 1 1 1 d d d d d d d d d d f 
+f f d d d d d d d d d d d d f f 
+9 f f d d d d d d d d d d d f 9 
+9 9 9 f f f d d d d d d f f f 9 
+9 9 9 9 9 f f f f f f f f 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
+    AaAAAAAAAAAaaaAAAAaaA.setPosition(AaAAAAAAAAAaaaAAAAaaA.x, 220)
+})
+let AaAAAAAAAAAaaaAAAAaaA: Sprite = null
+AaAAAAAAAAAaaaAAAAaaA = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f . . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . . f f . . . . 
+. . . . f f . . . . f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f . . . . . . f f . . . 
+. . f f f . . . . . . f f f . . 
+. . f f . . . . . . . . f f f . 
+. f f f . . . . . . . . . f f f 
+f f f . . . . . . . . . . . . f 
+`, SpriteKind.Player)
+scene.cameraFollowSprite(AaAAAAAAAAAaaaAAAAaaA)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100001010101010101010101010101010101010101010101010401010101040101010101040101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010104010101010101010104010104010101010101010101040101010101010101010101010101010101010101010101010101010401010101010104010101010101010101010101010101010101010101010101010101040101010101010101010101010101010101010101010104010101010104010101010101010101010101010101010101010101010101010101010202020202020202020202020202020203030303030303030303030303030303`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4],
+            TileScale.Sixteen
+        ))
+AaAAAAAAAAAaaaAAAAaaA.setPosition(10, 223)
+controller.moveSprite(AaAAAAAAAAAaaaAAAAaaA, 100, 100)
