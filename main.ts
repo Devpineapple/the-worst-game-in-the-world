c254c187e19ae821687ffd31e894a68ccc7237a8
T H A T  B O I <43853225+Devpineapple@users.noreply.github.com>
2020-04-01 10:26:51 -0400

    New! Enemy spawner (stasionary enemy spawner)

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Power_UP = SpriteKind.create()
+    export const Spawner = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -98,11 +99,49 @@ f f d d d d d d d d d d d d f f
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 `
 }
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    AaAAAAAAAAAaaaAAAAaaA.setImage(img`
+f f f f f f f f f f f f f f f f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 f 4 4 4 4 f 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 f f f f 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f f f f f f f f f f f f f f f f 
+`)
+})
 function Move_Player () {
     AaAAAAAAAAAaaaAAAAaaA.setPosition(10, 223)
     controller.moveSprite(AaAAAAAAAAAaaaAAAAaaA, 100, 100)
 }
 function Spawn_enemy () {
+    enemy_spawner = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f f f f f f f f f f f f f f . 
+. f f f f f f f f f f f f f f . 
+. f f 2 7 2 7 2 7 2 7 2 7 f f . 
+. f f 7 2 7 2 7 2 7 2 7 2 f f . 
+. f f 2 7 2 7 2 7 2 7 2 7 f f . 
+. f f 7 2 7 2 7 2 7 2 7 2 f f . 
+. f f 2 7 2 7 2 7 2 7 2 7 f f . 
+. f f 7 2 7 2 7 2 7 2 7 2 f f . 
+. f f 2 7 2 7 2 7 2 7 2 7 f f . 
+. f f 7 2 7 2 7 2 7 2 7 2 f f . 
+. f f 2 7 2 7 2 7 2 7 2 7 f f . 
+. f f f f f f f f f f f f f f . 
+. f f f f f f f f f f f f f f . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Spawner)
     Enemy_ = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -121,6 +160,112 @@ function Spawn_enemy () {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
+    Enemy_.setPosition(enemy_spawner.x, enemy_spawner.y)
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.destroy(effects.disintegrate, 500)
+    music.baDing.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    AaAAAAAAAAAaaaAAAAaaA.setImage(img`
+f f f f f f f f f f f f f f f f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 f 4 4 4 4 f 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 f f f f 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f f f f f f f f f f f f f f f f 
+`)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Power_UP, function (sprite, otherSprite) {
+    music.pewPew.play()
+    projectile = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . f 2 2 2 2 2 f . . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . . f 2 2 2 2 2 f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, AaAAAAAAAAAaaaAAAAaaA, 50, 100)
+    projectile.setFlag(SpriteFlag.StayInScreen, true)
+    projectile.setKind(SpriteKind.Projectile)
+    projectile.follow(Enemy_, 100)
+    otherSprite.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 220))
+})
+function Load_Game () {
+    color.setPalette(
+    color.Arcade
+    )
+    AaAAAAAAAAAaaaAAAAaaA = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 f 4 4 4 4 4 4 4 f 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 f f f f f f f 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Player)
+    scene.cameraFollowSprite(AaAAAAAAAAAaaaAAAAaaA)
+    tiles.setTilemap(tiles.createTilemap(
+            hex`1000100001010101010101010101010101010101010101010101010401010101040101010101040101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010104010101010101010104010104010101010101010101040101010101010101010101010101010101010101010101010101010401010101010104010101010101010101010101010101010101010101010101010101040101010101010101010101010101010101010101010104010101010104010101010101010101010101010101010101010101010101010101010202020202020202020202020202020203030303030303030303030303030303`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4],
+            TileScale.Sixteen
+        ))
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
+    AaAAAAAAAAAaaaAAAAaaA.setPosition(AaAAAAAAAAAaaaAAAAaaA.x, 223)
+})
+function Spawn_Powerups () {
     POwerup = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . f f f f f f f f f f f f . . 
@@ -199,144 +344,19 @@ function Spawn_enemy () {
     Powerup3.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 220))
     Powerup4.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 220))
 }
-function Load_Game () {
-    color.setPalette(
-    color.Arcade
-    )
-    AaAAAAAAAAAaaaAAAAaaA = sprites.create(img`
-f f f f f f f f f f f f f f f f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 f 4 4 4 4 4 4 4 f 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 f f f f f f f 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f f f f f f f f f f f f f f f f 
-`, SpriteKind.Player)
-    scene.cameraFollowSprite(AaAAAAAAAAAaaaAAAAaaA)
-    tiles.setTilemap(tiles.createTilemap(
-            hex`1000100001010101010101010101010101010101010101010101010401010101040101010101040101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010104010101010101010104010104010101010101010101040101010101010101010101010101010101010101010101010101010401010101010104010101010101010101010101010101010101010101010101010101040101010101010101010101010101010101010101010104010101010104010101010101010101010101010101010101010101010101010101010202020202020202020202020202020203030303030303030303030303030303`,
-            img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4],
-            TileScale.Sixteen
-        ))
-}
-function Spawn_Powerups () {
-	
-}
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    AaAAAAAAAAAaaaAAAAaaA.setImage(img`
-f f f f f f f f f f f f f f f f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 f 4 4 4 4 f 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 f f f f 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f f f f f f f f f f f f f f f f 
-`)
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy(effects.disintegrate, 500)
-    sprite.destroy(effects.disintegrate, 500)
-    music.baDing.play()
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    AaAAAAAAAAAaaaAAAAaaA.setImage(img`
-f f f f f f f f f f f f f f f f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 f 4 4 4 4 f 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 f f f f 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-f f f f f f f f f f f f f f f f 
-`)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Power_UP, function (sprite, otherSprite) {
-    music.pewPew.play()
-    projectile = sprites.createProjectileFromSprite(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . . f 2 2 2 2 2 f . . . . . 
-. . . f 2 2 2 2 2 2 2 f . . . . 
-. . . f 2 2 2 2 2 2 2 f . . . . 
-. . . f 2 2 2 2 2 2 2 f . . . . 
-. . . f 2 2 2 2 2 2 2 f . . . . 
-. . . f 2 2 2 2 2 2 2 f . . . . 
-. . . . f 2 2 2 2 2 f . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, AaAAAAAAAAAaaaAAAAaaA, 50, 100)
-    projectile.setFlag(SpriteFlag.StayInScreen, true)
-    projectile.setKind(SpriteKind.Projectile)
-    projectile.follow(Enemy_, 100)
-    otherSprite.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 220))
-})
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
-    AaAAAAAAAAAaaaAAAAaaA.setPosition(AaAAAAAAAAAaaaAAAAaaA.x, 223)
-})
-let projectile: Sprite = null
 let Powerup4: Sprite = null
 let Powerup3: Sprite = null
 let POWERUP2: Sprite = null
 let POwerup: Sprite = null
+let projectile: Sprite = null
 let Enemy_: Sprite = null
+let enemy_spawner: Sprite = null
 let AaAAAAAAAAAaaaAAAAaaA: Sprite = null
 game.splash("The worst game in the world", "PineappleDEV 2020")
 Load_Game()
-Move_Player()
 Spawn_enemy()
 Spawn_Powerups()
+Move_Player()
 game.onUpdateInterval(2000, function () {
     Enemy_ = sprites.create(img`
 . . . . . . . . . . . . . . . . 
@@ -356,7 +376,7 @@ game.onUpdateInterval(2000, function () {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
-    Enemy_.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 220))
+    Enemy_.setPosition(enemy_spawner.x, enemy_spawner.y)
     info.changeScoreBy(1)
 })
 game.onUpdate(function () {
